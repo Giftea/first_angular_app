7faa6980515ba8b56ab38f93c8d2799ff4e79d63
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-03-21 00:47:14 +0100

    made frontlett pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { AngBootStrapComponent } from './ang-boot-strap/ang-boot-strap.component';
+import { EmployeeInfoComponent } from './employee-info/employee-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: AngBootStrapComponent},
+  {path: "register", component: TextcomponentComponent},
+  {path: "login", component: EmployeeInfoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
